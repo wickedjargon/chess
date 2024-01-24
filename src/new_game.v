4248dd89c5d1830a506b94a64a06c241c98a40af
Farzin <farzineff@gmail.com>
2024-01-23 23:36:24 -0500

    placing pieces refactoring

--- src/new_game.v
+++ src/new_game.v
@@ -17,42 +17,36 @@ fn get_starting_pieces(color Color) []Piece {
 	return pieces
 }
 
-fn set_pieces_new_game(mut game_board [][]Piece) {
+fn set_pieces_back_rank(mut game_board [][]Piece, pieces []Piece, color Color, y_coord int) {
+	for x_coord, piece in pieces {
+		game_board[y_coord][x_coord] = piece
+	}
+}
+
+fn set_pieces_pawns(mut game_board [][]Piece, color Color, y_coord int) {
+	pawn := Piece{
+		shape: .pawn
+		color: color
+	}
+	for x_coord in 0 .. game_board_dimension {
+		game_board[y_coord][x_coord] = pawn
+	}
+}
+
+fn set_pieces(mut game_board [][]Piece) {
 	// setup the back-rank pieces
 	black_pieces := get_starting_pieces(Color.black)
-	mut y_coord := 0
-	for x_coord, piece in black_pieces {
-		game_board[y_coord][x_coord] = piece
-	}
-	y_coord = 7
 	white_pieces := get_starting_pieces(Color.white)
-	for x_coord, piece in white_pieces {
-		game_board[y_coord][x_coord] = piece
-	}
-
-	// setup the pawn pieces
-	y_coord = 1
-	black_pawn := Piece{
-		shape: .pawn
-		color: .black
-	}
-	for x_coord in 0 .. game_board_dimension {
-		game_board[y_coord][x_coord] = black_pawn
-	}
-	y_coord = 6
-	white_pawn := Piece{
-		shape: .pawn
-		color: .white
-	}
-	for x_coord in 0 .. game_board_dimension {
-		game_board[y_coord][x_coord] = white_pawn
-	}
+	set_pieces_back_rank(mut game_board, black_pieces, .black, 0)
+	set_pieces_back_rank(mut game_board, white_pieces, .black, 7)
+	set_pieces_pawns(mut game_board, .black, 1)
+	set_pieces_pawns(mut game_board, .white, 6)
 }
 
 fn new_game(mut app App) {
 	app.selection_state = .origin_coords
 	app.game_board.table = empty_game_board.clone()
-	set_pieces_new_game(mut app.game_board.table)
+	set_pieces(mut app.game_board.table)
 	dump(app.game_board.table)
 	app.game_board.current_player = .white
 	// set_pieces_new_game(mut app.game_board)
